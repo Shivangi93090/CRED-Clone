c52b59b1d0607fb782783c47d92d17d2c1009021
Abhinav Chauhan <abhinav.chauhan01@sap.com>
2024-06-07 21:11:56 +0530

    graphs

--- frontend/src/components/BarChart/index.jsx
+++ frontend/src/components/BarChart/index.jsx
@@ -52,7 +52,7 @@ const BarChart = (props) => {
               maintainAspectRatio: false,
               plugins: {
                 legend: {
-                  position: "top",
+                  position: "none",
                 },
               },
             }}
@@ -68,3 +68,240 @@ export default BarChart;
 
 
 
+// import React from 'react';
+// import { ResponsiveBar } from '@nivo/bar';
+
+// const BarChart = (props) => {
+//   const { labels, color, label, value } = props;
+
+//   // Transform data to match Nivo's expected format
+//   const data = labels.map((label, index) => ({
+//     label: label,
+//     value: value[index],
+//     color: color.background[index],
+//   }));
+
+//   return (
+//     <>
+//       {labels.length === 0 ? (
+//         <div className="text-center">
+//           <p
+//             style={{
+//               fontSize: '1rem',
+//               fontWeight: '400',
+//               marginTop: '1.5rem',
+//             }}
+//           >
+//             No data available to show.
+//           </p>
+//         </div>
+//       ) : (
+//         <div style={{ height: 400 }}>
+//           <ResponsiveBar
+//             data={data}
+//             keys={['value']}
+//             indexBy="label"
+//             margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+//             padding={0.3}
+//             valueScale={{ type: 'linear' }}
+//             indexScale={{ type: 'band', round: true }}
+//             colors={({ data }) => data.color}
+//             borderColor={{
+//               from: 'color',
+//               modifiers: [['darker', 1.6]],
+//             }}
+//             axisTop={null}
+//             axisRight={null}
+//             axisBottom={{
+//               tickSize: 5,
+//               tickPadding: 5,
+//               tickRotation: 0,
+//               legend: label,
+//               legendPosition: 'middle',
+//               legendOffset: 32,
+//             }}
+//             axisLeft={{
+//               tickSize: 5,
+//               tickPadding: 5,
+//               tickRotation: 0,
+//               legend: 'value',
+//               legendPosition: 'middle',
+//               legendOffset: -40,
+//             }}
+//             labelSkipWidth={12}
+//             labelSkipHeight={12}
+//             labelTextColor={{
+//               from: 'color',
+//               modifiers: [['darker', 1.6]],
+//             }}
+//             legends={[
+//               {
+//                 dataFrom: 'keys',
+//                 anchor: 'top',
+//                 direction: 'row',
+//                 justify: false,
+//                 translateX: 0,
+//                 translateY: -40,
+//                 itemsSpacing: 2,
+//                 itemWidth: 100,
+//                 itemHeight: 20,
+//                 itemDirection: 'left-to-right',
+//                 itemOpacity: 0.85,
+//                 symbolSize: 20,
+//                 effects: [
+//                   {
+//                     on: 'hover',
+//                     style: {
+//                       itemOpacity: 1,
+//                     },
+//                   },
+//                 ],
+//               },
+//             ]}
+//             animate={true}
+//             motionStiffness={90}
+//             motionDamping={15}
+//           />
+//         </div>
+//       )}
+//     </>
+//   );
+// };
+
+// export default BarChart;
+
+
+// import React from 'react';
+// import { ResponsiveBar } from '@nivo/bar';
+
+// const BarChart = (props) => {
+//   const { labels, color, label, value } = props;
+
+//   // Transform data to match Nivo's expected format
+//   const data = labels.map((label, index) => ({
+//     label: label,
+//     value: value[index],
+//     color: color.background[index],
+//   }));
+
+//   return (
+//     <>
+//       {labels.length === 0 ? (
+//         <div className="text-center">
+//           <p
+//             style={{
+//               fontSize: '1rem',
+//               fontWeight: '400',
+//               marginTop: '1.5rem',
+//             }}
+//           >
+//             No data available to show.
+//           </p>
+//         </div>
+//       ) : (
+//         <div style={{ height: 400 }}>
+//           <ResponsiveBar
+//             data={data}
+//             keys={['value']}
+//             indexBy="label"
+//             margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+//             padding={0.3}
+//             valueScale={{ type: 'linear' }}
+//             indexScale={{ type: 'band', round: true }}
+//             colors={{ scheme: 'nivo' }}
+//             defs={[
+//               {
+//                 id: 'dots',
+//                 type: 'plain',
+//                 background: 'inherit',
+//                 color: '#38bcb2',
+//                 size: 4,
+//                 padding: 1,
+//                 stagger: true,
+//               },
+//               {
+//                 id: 'lines',
+//                 type: 'patternLines',
+//                 background: 'inherit',
+//                 color: '#eed312',
+//                 rotation: -45,
+//                 lineWidth: 6,
+//                 spacing: 10,
+//               },
+//             ]}
+//             fill={[
+//               {
+//                 match: {
+//                   id: 'value',
+//                 },
+//                 id: 'dots',
+//               },
+//             ]}
+//             borderColor={{
+//               from: 'color',
+//               modifiers: [['darker', 1.6]],
+//             }}
+//             axisTop={null}
+//             axisRight={null}
+//             axisBottom={{
+//               tickSize: 5,
+//               tickPadding: 5,
+//               tickRotation: 0,
+//               legend: label,
+//               legendPosition: 'middle',
+//               legendOffset: 32,
+//               truncateTickAt: 0
+//             }}
+//             axisLeft={{
+//               tickSize: 5,
+//               tickPadding: 5,
+//               tickRotation: 0,
+//               legend: 'value',
+//               legendPosition: 'middle',
+//               legendOffset: -40,
+//               truncateTickAt: 0
+//             }}
+//             labelSkipWidth={12}
+//             labelSkipHeight={12}
+//             labelTextColor={{
+//               from: 'color',
+//               modifiers: [['darker', 1.6]],
+//             }}
+//             legends={[
+//               {
+//                 dataFrom: 'keys',
+//                 anchor: 'bottom-right',
+//                 direction: 'column',
+//                 justify: false,
+//                 translateX: 120,
+//                 translateY: 0,
+//                 itemsSpacing: 2,
+//                 itemWidth: 100,
+//                 itemHeight: 20,
+//                 itemDirection: 'left-to-right',
+//                 itemOpacity: 0.85,
+//                 symbolSize: 20,
+//                 effects: [
+//                   {
+//                     on: 'hover',
+//                     style: {
+//                       itemOpacity: 1,
+//                     },
+//                   },
+//                 ],
+//               },
+//             ]}
+//             animate={true}
+//             motionStiffness={90}
+//             motionDamping={15}
+//             role="application"
+//             ariaLabel="Nivo bar chart demo"
+//             barAriaLabel={e => `${e.id}: ${e.formattedValue} in label: ${e.indexValue}`}
+//           />
+//         </div>
+//       )}
+//     </>
+//   );
+// };
+
+// export default BarChart;
--- frontend/src/components/PieChart/index.jsx
+++ frontend/src/components/PieChart/index.jsx
@@ -1,23 +1,64 @@
-import { Pie } from "react-chartjs-2";
-import { Chart, ArcElement, Tooltip, Legend } from "chart.js";
 
-Chart.register(ArcElement, Tooltip, Legend);
+import React from 'react';
+import { ResponsivePie } from '@nivo/pie';
 
 const PieChart = (props) => {
   const { labels, color, label, value } = props;
 
-  const data = {
-    labels: labels,
-    datasets: [
-      {
-        label: label,
-        data: value,
-        backgroundColor: color.background,
-        borderColor: color.border,
-        borderWidth: 1,
-      },
-    ],
-  };
+  // Transform data to match Nivo's expected format
+  const data = labels.map((label, index) => ({
+    id: label,
+    label: label,
+    value: value[index],
+    color: color.background[index],
+  }));
+
+//   const legendsPerRow = 5; // Number of legends per row
+//   const containerWidth = 1000; // Width of the container
+
+//   // Function to split legends into multiple rows
+//   const createLegendConfig = (data) => {
+//     const rows = Math.ceil(data.length / legendsPerRow);
+//     const legends = [];
+//     const legendWidth = containerWidth / legendsPerRow; // Calculate dynamic width of legend item
+
+//     for (let i = 0; i < rows; i++) {
+//       const startIndex = i * legendsPerRow;
+//       const endIndex = startIndex + legendsPerRow;
+//       const rowLegends = data.slice(startIndex, endIndex).map(d => ({ id: d.id, label: d.label, color: d.color }));
+
+//       legends.push({
+//         anchor: 'bottom',
+//         direction: 'row',
+//         justify: false,
+//         translateX: 0,
+//         translateY: 36 + (i * 20), // Adjust translateY to stack rows
+//         itemsSpacing: 1,
+//         itemWidth: legendWidth,
+//         itemHeight: 20,
+//         itemTextColor: '#999',
+//         itemDirection: 'left-to-right',
+//         itemOpacity: 0.85,
+//         symbolSize: 8,
+//         symbolShape: 'circle',
+//         effects: [
+//           {
+//             on: 'hover',
+//             style: {
+//               itemTextColor: '#000',
+//             },
+//           },
+//         ],
+//         data: rowLegends,
+//       });
+//     }
+
+//     return legends;
+//   };
+
+//   // Get dynamic legend configuration
+//   const legends = createLegendConfig(data);
+
 
   return (
     <>
@@ -34,12 +75,225 @@ const PieChart = (props) => {
           </p>
         </div>
       ) : (
-        <div
-        >
-          <Pie
+        <div style={{ height: 350}}>
+          <ResponsivePie
             data={data}
-            height={400}
-            options={{ maintainAspectRatio: false }}
+            margin={{ top: 40, right: 80, bottom: 100, left: 70 }}
+            innerRadius={0.5}
+            padAngle={0.7}
+            cornerRadius={3}
+            activeOuterRadiusOffset={15}
+            borderWidth={1}
+            borderColor={{
+              from: 'color',
+              modifiers: [['darker', 0.2]],
+            }}
+            arcLinkLabelsSkipAngle={10}
+            arcLinkLabelsTextColor="#333333"
+            arcLinkLabelsThickness={2}
+            arcLinkLabelsColor={{ from: 'color' }}
+            arcLabelsSkipAngle={10}
+            arcLabelsTextColor={{
+              from: 'color',
+              modifiers: [['darker', 2]],
+            }}
+            defs={[
+              {
+                id: 'dots',
+                type: 'patternDots',
+                background: 'inherit',
+                color: 'rgba(255, 255, 255, 0.3)',
+                size: 4,
+                padding: 1,
+                stagger: true,
+              },
+              {
+                id: 'lines',
+                type: 'patternLines',
+                background: 'inherit',
+                color: 'rgba(255, 255, 255, 0.3)',
+                rotation: -45,
+                lineWidth: 6,
+                spacing: 10,
+              },
+            ]}
+            // fill={[
+            //   {
+            //     match: {
+            //       id: 'ruby',
+            //     },
+            //     id: 'dots',
+            //   },
+            //   {
+            //     match: {
+            //       id: 'c',
+            //     },
+            //     id: 'dots',
+            //   },
+            //   {
+            //     match: {
+            //       id: 'go',
+            //     },
+            //     id: 'dots',
+            //   },
+            //   {
+            //     match: {
+            //       id: 'python',
+            //     },
+            //     id: 'dots',
+            //   },
+            //   {
+            //     match: {
+            //       id: 'scala',
+            //     },
+            //     id: 'lines',
+            //   },
+            //   {
+            //     match: {
+            //       id: 'lisp',
+            //     },
+            //     id: 'lines',
+            //   },
+            //   {
+            //     match: {
+            //       id: 'elixir',
+            //     },
+            //     id: 'lines',
+            //   },
+            //   {
+            //     match: {
+            //       id: 'javascript',
+            //     },
+            //     id: 'lines',
+            //   },
+            // ]}
+            // legends={[
+            //   {
+            //     anchor: 'bottom',
+            //     direction: 'row',
+            //     justify: false,
+            //     translateX: 0,
+            //     translateY: 56,
+            //     itemsSpacing: 0,
+            //     itemWidth: 100,
+            //     itemHeight: 18,
+            //     itemTextColor: '#999',
+            //     itemDirection: 'top-to-bottom',
+            //     itemOpacity: 1,
+            //     symbolSize: 10,
+            //     symbolShape: 'circle',
+            //     effects: [
+            //       {
+            //         on: 'hover',
+            //         style: {
+            //           itemTextColor: '#000',
+            //         },
+            //       },
+            //     ],
+            //   },
+            // ]}
+            // legends={[
+            //   {
+            //     anchor: 'bottom',
+            //     direction: 'row',
+            //     justify: false,
+            //     translateX: 0,
+            //     translateY: 36,
+            //     itemsSpacing: 0,
+            //     itemWidth: 100,
+            //     itemHeight: 18,
+            //     itemTextColor: '#999',
+            //     itemDirection: 'left-to-right',
+            //     itemOpacity: 1,
+            //     symbolSize: 18,
+            //     symbolShape: 'circle',
+            //     effects: [
+            //       {
+            //         on: 'hover',
+            //         style: {
+            //           itemTextColor: '#000',
+            //         },
+            //       },
+            //     ],
+            //   },
+            //   {
+            //     anchor: 'bottom',
+            //     direction: 'row',
+            //     justify: false,
+            //     translateX: 0,
+            //     translateY: 56,
+            //     itemsSpacing: 0,
+            //     itemWidth: 100,
+            //     itemHeight: 18,
+            //     itemTextColor: '#999',
+            //     itemDirection: 'left-to-right',
+            //     itemOpacity: 1,
+            //     symbolSize: 18,
+            //     symbolShape: 'circle',
+            //     effects: [
+            //       {
+            //         on: 'hover',
+            //         style: {
+            //           itemTextColor: '#000',
+            //         },
+            //       },
+            //     ],
+            //   },
+            // ]}
+
+            // legends={[
+            //   {
+            //     dataFrom: 'keys',
+            //     anchor: 'bottom',
+            //     direction: 'row',
+            //     justify: false,
+            //     translateX: 0,
+            //     translateY: 36,
+            //     itemsSpacing: 2,
+            //     itemWidth: 100,
+            //     itemHeight: 20,
+            //     itemTextColor: '#999',
+            //     itemDirection: 'left-to-right',
+            //     itemOpacity: 0.85,
+            //     symbolSize: 20,
+            //     symbolShape: 'circle',
+            //     effects: [
+            //       {
+            //         on: 'hover',
+            //         style: {
+            //           itemTextColor: '#000',
+            //         },
+            //       },
+            //     ],
+            //     data: firstRow.map(d => ({ id: d.id, label: d.label, color: d.color }))
+            //   },
+            //   {
+            //     dataFrom: 'keys',
+            //     anchor: 'bottom',
+            //     direction: 'row',
+            //     justify: false,
+            //     translateX: 0,
+            //     translateY: 56,
+            //     itemsSpacing: 2,
+            //     itemWidth: 100,
+            //     itemHeight: 20,
+            //     itemTextColor: '#999',
+            //     itemDirection: 'left-to-right',
+            //     itemOpacity: 0.85,
+            //     symbolSize: 20,
+            //     symbolShape: 'circle',
+            //     effects: [
+            //       {
+            //         on: 'hover',
+            //         style: {
+            //           itemTextColor: '#000',
+            //         },
+            //       },
+            //     ],
+            //     data: secondRow.map(d => ({ id: d.id, label: d.label, color: d.color }))
+            //   },
+            // ]}
+            // legends={legends}
           />
         </div>
       )}
@@ -48,3 +302,127 @@ const PieChart = (props) => {
 };
 
 export default PieChart;
+
+// import React from 'react';
+// import { ResponsivePie } from '@nivo/pie';
+
+// const PieChart = (props) => {
+//   const { data } = props;
+
+//   return (
+//     <>
+//       {data.length === 0 ? (
+//         <div className="text-center">
+//           <p
+//             style={{
+//               fontSize: '1rem',
+//               fontWeight: '400',
+//               marginTop: '1.5rem',
+//             }}
+//           >
+//             No data available to show.
+//           </p>
+//         </div>
+//       ) : (
+//         <div style={{ height: 400 }}>
+//           <ResponsivePie
+//             data={data}
+//             margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+//             innerRadius={0.5}
+//             padAngle={0.7}
+//             cornerRadius={3}
+//             activeOuterRadiusOffset={8}
+//             borderWidth={1}
+//             borderColor={{
+//               from: 'color',
+//               modifiers: [['darker', 0.2]],
+//             }}
+//             arcLinkLabelsSkipAngle={10}
+//             arcLinkLabelsTextColor="#333333"
+//             arcLinkLabelsThickness={2}
+//             arcLinkLabelsColor={{ from: 'color' }}
+//             arcLabelsSkipAngle={10}
+//             arcLabelsTextColor={{
+//               from: 'color',
+//               modifiers: [['darker', 2]],
+//             }}
+//             defs={[
+//               {
+//                 id: 'dots',
+//                 type: 'patternDots',
+//                 background: 'inherit',
+//                 color: 'rgba(255, 255, 255, 0.3)',
+//                 size: 4,
+//                 padding: 1,
+//                 stagger: true,
+//               },
+//               {
+//                 id: 'lines',
+//                 type: 'patternLines',
+//                 background: 'inherit',
+//                 color: 'rgba(255, 255, 255, 0.3)',
+//                 rotation: -45,
+//                 lineWidth: 6,
+//                 spacing: 10,
+//               },
+//             ]}
+//             fill={[
+//               {
+//                 match: {
+//                   id: 'ruby',
+//                 },
+//                 id: 'dots',
+//               },
+//               {
+//                 match: {
+//                   id: 'c',
+//                 },
+//                 id: 'dots',
+//               },
+//               {
+//                 match: {
+//                   id: 'go',
+//                 },
+//                 id: 'dots',
+//               },
+//               {
+//                 match: {
+//                   id: 'python',
+//                 },
+//                 id: 'dots',
+//               },
+//               {
+//                 match: {
+//                   id: 'scala',
+//                 },
+//                 id: 'lines',
+//               },
+//               {
+//                 match: {
+//                   id: 'lisp',
+//                 },
+//                 id: 'lines',
+//               },
+//               {
+//                 match: {
+//                   id: 'elixir',
+//                 },
+//                 id: 'lines',
+//               },
+//               {
+//                 match: {
+//                   id: 'javascript',
+//                 },
+//                 id: 'lines',
+//               },
+//             ]}
+            
+//           />
+//         </div>
+//       )}
+//     </>
+//   );
+// };
+
+// export default PieChart;
+
